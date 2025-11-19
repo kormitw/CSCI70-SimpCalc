fcccbac15b15d3e814eb76ad943718662d915201
kormitw <ramunix@Kormit-Laptop.>
2025-11-19 15:22:01 +0800

    Able to identify key identifiers

--- scanner.cpp
+++ scanner.cpp
@@ -8,17 +8,29 @@
 using namespace std;
 
 map<int, string> tokens_dict = {
-    {13,"newline"}, // newline
-    {10,"newline"}, // newline
-    {47,"divide"}, // "/"
-    {58,"colon"}, // ":"
-    {61,"equals"}, // "="
-    {42,"multiply"} // "*"
+    {13,"Newline"}, // newline
+    {10,"Newline"}, // newline
+    {47,"Divide"}, // "/"
+    {58,"Colon"}, // ":"
+    {61,"Equals"}, // "="
+    {42,"Multiply"}, // "*"
+    {45,"Minus"}, // "-"
+    {40,"LeftParen"}, // "("
+    {41,"RightParen"}, // ")"
+    {59,"Semicolon"} // ")\;"
 };
 
+map<string,string> key_identifiers = {
+    {"IF","If"}
+};
 
-
-
+void release(string &state, string &token){ //Function to release state and token
+    if (!token.empty()) {
+        cout << state << " " << token << endl;
+        token.clear();
+        state.clear();
+    }
+}
 void gettoken(fstream &input_file){ //Gets the next token from the input
     string state;
     string map;
@@ -27,13 +39,13 @@ void gettoken(fstream &input_file){ //Gets the next token from the input
 
     while (input_file.get(in_char)) {
         map = tokens_dict[(int)in_char];
-        if (map == "newline"){
+        if (map == "Newline"){
             in_char = ' ';
         }
 
-        if(state != "skip"){ //checks for state
-            if (state == "divide" && map == "divide"){ //Comment
-                state = "skip";
+        if(state != "Skip"){ //checks for state
+            if (state == "Divide" && map == "Divide"){ //Comment
+                state = "Skip";
             }
             else if(isalpha(in_char) || in_char == '_'){ //Identifier
                 state = "Identifier";
@@ -43,11 +55,20 @@ void gettoken(fstream &input_file){ //Gets the next token from the input
                 state = "Identifier";
                 token += in_char;
             }
-            else if(isdigit(in_char)){
-                state = "Num";
+            else if(isdigit(in_char)){ //Number
+                if(state != "Number"){
+                    release(state, token);
+                    state = "Number";
+                }
                 token += in_char;
             }
-            else if(map == "multiply"){
+            else if(state == "Number" && in_char == '.'){ //decimal
+                token += in_char;
+            }
+            else if(map == "Multiply"){ //Multiply
+                if(map != state){
+                    release(state, token);
+                }
                 if(state == map){
                     state = "Raise";
                     token += in_char;
@@ -57,26 +78,53 @@ void gettoken(fstream &input_file){ //Gets the next token from the input
                     token += in_char;
                 }
             }
-            else if(map == "colon"){ //Colon
+            else if(map == "Colon"){ //Colon
                 state = map;
                 token += in_char;
             }
-            else if(map == "equals"){ //Equals
+            else if(map == "Equals"){ //Equals
                 token += in_char;
-                if(state == "colon"){
+                if(state == "Colon"){
                     state = "Assign";
                 }
-            }
-            else { //release
-                if (token != ""){
-                    cout << state <<" " << token << endl;
-                    token = "";
+                else{
+                    state = map;
                 }
+            }
+            else if(map == "Minus"){ //Minus
+                if(state != map && state != ""){
+                    release(state,token);
+                }
+                state = map;
+                token += in_char;
+            }
+            else if(map == "LeftParen"){ //LeftParen
+                release(state,token);
+                state = map;
+                token += in_char;
+            }
+            else if(map == "RightParen"){ //RightParen
+                release(state,token);
+                state = map;
+                token += in_char;
+            }
+            else if(map == "Semicolon"){ //Semicolon
+                release(state,token);
+                state = map;
+                token += in_char;
+            }
+            else if(map != state){ //Release
+                if(state == "Identifier"){
+                    if(key_identifiers.find(token) != key_identifiers.end()){
+                        state = key_identifiers[token];
+                    }
+                }
+                release(state, token);
                 state = map;
             }
         }
 
-        else if(map == "newline"){ //resets skip when newline
+        else if(map == "Newline"){ //resets skip when newline
             state = map;
         }
 
@@ -84,10 +132,6 @@ void gettoken(fstream &input_file){ //Gets the next token from the input
         // cout << " State: " << state << endl;
     }
 
-    void release(){
-        cout << state <<" " << token << endl;
-        token = "";
-    }
 }
 
 int main(){
