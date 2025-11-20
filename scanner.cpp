340015de9ab5b2cc75496e270dee22c563d5750a
kormitw <ramunix@Kormit-Laptop.>
2025-11-20 21:36:14 +0800

    added code COA

--- scanner.cpp
+++ scanner.cpp
@@ -1,3 +1,19 @@
+/**
+@author Roger II Candari (221291)
+@version Noveber 20, 2025
+**/
+/*
+I have not discussed the C++ language code in my program 
+with anyone other than my instructor or the teaching assistants 
+assigned to this course.
+I have not used C++ language code obtained from another student, 
+or any other unauthorized source, either modified or unmodified.
+If any C++ language code or documentation used in my program 
+was obtained from another source, such as a textbook or website, 
+that has been clearly noted with a proper citation in the comments
+of my program.
+*/
+
 #include <stdio.h>
 #include <string>
 #include <fstream>
